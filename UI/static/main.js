e6d43fa63a46c5f7afab24828234591b7c5e8b8d
Arusey <kevinkibetw7@gmail.com>
2018-10-10 21:30:03 +0300

    [Feature

--- UI/static/main.js
+++ UI/static/main.js
@@ -1,22 +1,31 @@
-var mymodal = document.getElementsByClassName("mymodal")[0];
-var infobtn = document.querySelectorAll("#infobtn");
-var closeModal = document.querySelectorAll(".close-modal");
-var addcart = document.querySelectorAll("addcart")
+let mymodal = document.getElementsByClassName("mymodal")[0];
+let infobtn = document.querySelectorAll("#infobtn");
+let closeModal = document.querySelectorAll(".close-modal");
+let addcart = document.getElementsByClassName("addcart")["0"];
+ 
+let salemodal = document.getElementsByClassName("sale-modal")[0];
 
-// let details = document.getElementById("description");
-// let close = document.querySelector(".close");
-// let closeModal = document.querySelector(".close-modal");
-// let addcart = document.querySelector("cart");
-//function will display modal inline when clicked
 function displayModal() {
     mymodal.style.display = "block";
 }
+function saleModal() {
 
-// closeModal.onclick = function(){
-//     mymodal.style.display = "none"
+    salemodal.style.display = "block"
+}
+// function addCart() {
+//     addcart
 // }
+
+
 window.onclick = function (event) {
-    if (event.target === mymodal) {
+    if (event.target === mymodal || event.target === salemodal) {
         mymodal.style.display = "none";
+        salemodal.style.display = "none";
     }
 }
+mymodal.style.display = "none"
+// window.onclick = function (event) {
+//     if (event.target === salemodal) {
+//         salemodal.style.display = "none";
+//     }
+// }
