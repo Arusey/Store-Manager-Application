e4b45e3c8f46d5f4e0c2abf8851671ddb32b2c06
Arusey <kevinkibetw7@gmail.com>
2018-11-11 09:14:39 +0300

    [Feature #161863856]Add get all users endpoint and consume it

---
+++ UI/static/getUsers.js
@@ -0,0 +1,40 @@
+window.onload = getAllusers()
+function getAllusers(){
+    let token = localStorage.getItem('token');
+    let allusers = document.getElementById('allusers')
+
+    fetch('http://127.0.0.1:5000/api/v2/auth/attsignup', {
+        headers: {
+            'x-access-token': token
+        }
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        if (data.Message == 'This token is invalid'){
+            alert('Please login first');
+            window.location.replace('index.html')
+        }
+        let result = '';
+        data.users.forEach(user => {
+            result += `
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    
+                </tr>
+                <tr>
+                    <td>${user.name}</td>
+                    <td>${user.email}</td>
+                    <td>${user.role}</td>
+
+                    <td><span><i class="far fa-trash-alt"></i></span></td>
+                </tr>
+            `
+            allusers.innerHTML = result;
+        });
+        localStorage.setItem('users', JSON.stringify(data.users));
+    })
+    .catch((error) => console.log(error))
+}
